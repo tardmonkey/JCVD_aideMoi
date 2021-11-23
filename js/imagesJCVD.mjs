3ddf937b94bf93907de987db7c4bb5687d5f8745
tardmonkey <guillain.benoist@gmail.com>
2021-11-23 20:31:15 +0100

    shit is real

---
+++ js/imagesJCVD.mjs
@@ -0,0 +1,7 @@
+export const imagesJCVD = [
+    
+    "img/JCVD/9.gif",
+    "img/JCVD/11.gif",
+    "img/JCVD/12.gif",
+    
+]
--- js/script.js
+++ js/script.js
@@ -1,24 +1,44 @@
 import { citations } from "./citation.mjs";
+import { imagesJCVD } from "./imagesJCVD.mjs";
 
 const button = document.querySelector("button");
 const p = document.querySelector("p")
-button.addEventListener("click",balancerCitation)
+const img = document.querySelector("img")
+button.addEventListener("click",leGrandClick)
 
 /* 
     @return {string} randomCitation
 */
-function selectCitation(){
-    let random = ~~(Math.random() * citations.length);
-    let randomCitation = '<p>' + citations[random] + '</p>' + '<span>by Jean-Claude Van Damme</span>';
-    console.log(randomCitation);
-    return randomCitation;
+function leGrandClick(){
+    function selectCitation(){
+        let random = ~~(Math.random() * citations.length);
+        let randomCitation = '<p>"' + citations[random] + '"</p>' + '<span>by Jean-Claude Van Damme</span>';
+        return randomCitation;
+        
+    }
+
+    function balancerCitation(){
+        
+        p.innerHTML = selectCitation();
+    }
+
+
+    /* 
+        @return {string} randomImage
+    */
+    function selectImage(){
+        let random = ~~(Math.random() * imagesJCVD.length);
+        let randomImage = imagesJCVD[random];
+        return randomImage;
+        
+    }
+    function balancerImage(){
+        
+        img.src = selectImage();
+    }
+    balancerCitation()
+    balancerImage()
+
     
 }
-selectCitation()
-
-function balancerCitation(){
-    
-    p.innerHTML = selectCitation();
-}
-
 
