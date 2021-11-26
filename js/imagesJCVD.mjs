00f98590a33f2bd8e94ba4859c671b8f122bb2fb
tardmonkey <guillain.benoist@gmail.com>
2021-11-27 00:26:02 +0100

    ajout bg + retaf tailles images et durée animations

--- js/imagesJCVD.mjs
+++ js/imagesJCVD.mjs
@@ -1,7 +1,10 @@
 export const imagesJCVD = [
     
-    "img/JCVD/2.gif",
     "img/JCVD/1.gif",
+    "img/JCVD/2.gif",
+    "img/JCVD/3.gif",
+    "img/JCVD/4.gif",
+    "img/JCVD/6.gif",
     "img/JCVD/9.gif",
     "img/JCVD/11.gif",
     "img/JCVD/12.gif",
